9be2989a161fa0f478487dddf4e33fa6524f82ea
Eskandar011 <alejandro.lima11vaz@gmail.com>
2023-10-02 16:42:06 -0600

    Inicio de la parte 1 de React

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,66 +1,35 @@
-const Header = (props) => {
-  console.log(props.course.name);
-  return <h1>{props.course.name}</h1>;
-};
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const Content = (props) => {
-  const parts = props.course.parts;
-  return parts.map((element) => (
-    <>
-      <Part part={element} />
-    </>
-  ));
-};
-const Part = (props) => {
-  const part = props.part;
+function App() {
+  const [count, setCount] = useState(0)
 
-  return (
-    <div>
-      <p>
-        {part.name} {part.exercises}
-      </p>
-    </div>
-  );
-};
-
-const Total = (props) => {
-  const parts = props.course.parts;
   return (
     <>
-      <p>
-        Number of exercises{" "}
-        {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
       </p>
     </>
-  );
-};
+  )
+}
 
-const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
-
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  );
-};
-
-export default App;
+export default App
